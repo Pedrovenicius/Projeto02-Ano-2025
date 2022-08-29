8fe7c34798d9e705298e28ec1505e8ccee5a6298
Pedrovenicius <112330605+Pedrovenicius@users.noreply.github.com>
2022-08-29 09:13:52 -0300

    Add files via upload

---
+++ desafio2.py
@@ -0,0 +1,21 @@
+print ("quantos anos você tem")
+nascimento = input()
+nascimento = int(nascimento)
+print ("Se você fosse um cachorro, você teria", nascimento *7, "anos!")
+print('''
+    |-----------------|
+\  /|                 | \  /
+ \/ |    (@)    (@)   |  \/
+     \        T      /
+      \             /
+       \     &     /----------------------|
+        \---------/                       ~~\
+           \                              ~~\\
+            \                             |  \\
+             \                            |   \\
+              \______________________     |     -
+               ||  |                  ||  |
+               ||  |                  ||  | 
+              ((___|                 ((___|
+
+ ''')             
