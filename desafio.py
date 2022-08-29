ec1fbd6df46b981314441e4df95cfe5a18a7eb44
Pedrovenicius <112330605+Pedrovenicius@users.noreply.github.com>
2022-08-29 09:14:59 -0300

    Add files via upload

---
+++ desafio.py
@@ -0,0 +1,7 @@
+print ( "Em que ano você nasceu?")
+nascimento = input()
+nascimento = int(nascimento)
+print ( "Para qual ano você quer saber sua idade ?")
+saber = input ()
+saber = int(saber)
+print ("No ano", saber,"você tera",saber - nascimento,"anos!"  ) 
